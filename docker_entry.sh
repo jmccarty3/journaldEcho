8615d7079a5215ce98d134d5eeca03bec2f10bc9
Justin McCarty <jmccarty@ip-192-168-1-11.ec2.internal>
2016-02-12 10:56:46 -0500

    Allowing for more complext options to be passed

--- docker_entry.sh
+++ docker_entry.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-OPTIONS=${@:-follow}
+OPTIONS="${@:-follow}"
 hostip=$(ip route show | awk '/default/ {print $3}')
 
-COMMAND="http://$hostip:$GATEWAY_PORT/entries?$OPTIONS"
+COMMAND="curl http://$hostip:$GATEWAY_PORT/entries?$OPTIONS"
 
 echo "Running $COMMAND"
-curl $COMMAND
+eval $COMMAND
